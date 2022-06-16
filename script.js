e5fb42a9043cf2f991a9d39075b011fa6649f495
Alireza <alirezwa03@gmail.com>
2022-06-16 09:24:24 +0430

    handler script fot todo trush and complete

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-const input = document.querySelector('.todoInput');
-const button = document.querySelector('.addTodo');
-const todoInput = document.querySelector('.todoListInput');
-const todoList = document.querySelector('.todoList');
+const input = document.querySelector(".todoInput");
+const button = document.querySelector(".addTodo");
+const todoInput = document.querySelector(".todoListInput");
+const todoList = document.querySelector(".todoList");
+const doneTodo = document.querySelector(".doneTodo");
+const faCheck = document.querySelector('.fa-check');
 
-button.addEventListener('click', () => {
+
+function todoHandler() {
   let inputValue = input.value;
-  const todo = document.createElement('div');
-  todo.classList.add('todo');
-  const markup = `
-  <div class="doneTodo">
-    <i class="fa-solid fa-check"></i>
-  </div>
-  <input type="text" class="todoListInput">
-  <button class="deleteTodo">
-    <i class="fa-solid fa-trash-can"></i>
-  </button>
-  `
-  todo.innerHTML  = markup;
-  todoList.appendChild(todo)
-})
+  if (inputValue !== "") {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const doneTodoDiv = document.createElement('div');
+    doneTodoDiv.classList.add('doneTodo');
+    doneTodoDiv.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneTodoDiv);
+
+    
+    const newInput = document.createElement('input');
+    newInput.classList.add('todoListInput');
+    newInput.value = inputValue;
+    todo.appendChild(newInput)
+    
+    const trashDiv = document.createElement('button');
+    trashDiv.classList.add('deleteTodo')
+    trashDiv.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    todo.appendChild(trashDiv);
+    
+    todoList.appendChild(todo);
+    input.value = "";
+  }
+}
+
+button.addEventListener("click", () => {
+  todoHandler();
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    todoHandler();
+  }
+});
+
+todoList.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.classList.contains('fa-check')) {
+    e.target.parentElement.parentElement.classList.toggle('doneTodoShow');
+  } else if (e.target.classList.contains('fa-trash-can')) {
+    e.target.parentElement.parentNode.classList.add('deleteTodoShow');
+    setTimeout(() => {e.target.parentElement.parentElement.remove()}, 400)
+  }
+});
